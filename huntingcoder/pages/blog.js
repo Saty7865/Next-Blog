55b7b2f2d2d956c923c7896160010d4613e70655
Saty <saty7865@gmail.com>
2023-05-17 19:09:06 +0530

    tut15 completed

--- huntingcoder/pages/blog.js
+++ huntingcoder/pages/blog.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 const blog = () => {
   return (
     <div>
-      this is blog
+      <main className={styles.main}>
+        <div className="blogItem1">
+          <Link href={"blogpost/learn-javascript"}>
+            <h3>How to learn JavaScript in 2022?</h3>
+          </Link>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur commodi consectetur eos.</p>
+        </div>
+        <div className="blogItem2">
+          <h3>How to learn JavaScript in 2022?</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur commodi consectetur eos.</p>
+        </div>
+        <div className="blogItem3">
+          <h3>How to learn JavaScript in 2022?</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur commodi consectetur eos.</p>
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default blog
+export default blog;
--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,10 +1,19 @@
 import React from "react";
+import styles from "../../styles/BlogPost.module.css";
 import { useRouter } from "next/router";
 
 const Slug = () => {
   const router = useRouter();
-  const {slug} = router.query;
-  return <div>{slug}</div>;
+  const { slug } = router.query;
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Title of the page {slug}</h1>
+        <hr />
+        <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, architecto quaerat tenetur blanditiis consectetur sint voluptatem at maxime qui, repellat necessitatibus aliquam quis.</div>
+      </main>
+    </div>
+  );
 };
 
 export default Slug;
