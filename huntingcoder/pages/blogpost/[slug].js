8b41cda97e61dbda5abdfaaa4be53a3e64a5d98d
Saty <saty7865@gmail.com>
2023-05-21 13:44:09 +0530

    tut23 completed

--- huntingcoder/pages/blogpost/[slug].js
+++ huntingcoder/pages/blogpost/[slug].js
@@ -1,32 +1,47 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/BlogPost.module.css";
 import { useRouter } from "next/router";
+import * as fs from "node:fs";
 
 //Step1: Find the file Corresponding to the slug
 //Step2: Populate them inside the page
 
-export const getServerSideProps = async (context) => {
-  const { slug } = context.query;
-  const data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`);
-  const myBlog = await data.json();
-  return { props: { myBlog } };
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          name: "next.js",
+          slug: "how-to-learn-django",
+        },
+        params: {
+          name: "next.js",
+          slug: "how-to-learn-flask",
+        },
+        params: {
+          name: "next.js",
+          slug: "how-to-learn-javascript",
+        },
+        params: {
+          name: "next.js",
+          slug: "how-to-learn-nextjs",
+        },
+      }, // See the "paths" section below
+    ],
+    fallback: true, // false or "blocking"
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { slug } = context.params;
+  
+  let myBlog = await fs.promises.readFile(`blogdata/${slug}.json`, "utf-8");
+
+  return { props: { myBlog:JSON.parse(myBlog) } };
 };
 
 const Slug = (props) => {
   const [blog, setBlog] = useState(props.myBlog);
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   if (!router.isReady) return;
-  //   const { slug } = router.query;
-  //   fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
-  //     .then((a) => {
-  //       return a.json();
-  //     })
-  //     .then((parsed) => {
-  //       setBlog(parsed);
-  //     });
-  // }, [router.isReady]);
 
   return (
     <div className={styles.container}>
