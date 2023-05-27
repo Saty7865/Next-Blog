ffc5980eaf83652d321999cae309d324020e253a
Saty <saty7865@gmail.com>
2023-05-27 20:23:35 +0530

    tut30 completed

--- huntingcoder/pages/api/blogs.js
+++ huntingcoder/pages/api/blogs.js
@@ -1,17 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as fs from "node:fs";
 
 export default async function handler(req, res) {
   let data = await fs.promises.readdir("blogdata");
   let allCount = data.length;
-  // console.log(data)
   let myfile;
   let allBlogs = [];
 
   for (let index = 0; index < req.query.count; index++) {
     const item = data[index];
     myfile = await fs.promises.readFile("blogdata/" + item, "utf-8");
-    // console.log(myfile)
     allBlogs.push(JSON.parse(myfile));
   }
   res.status(200).json({ allCount, allBlogs });
--- huntingcoder/pages/api/getblog.js
+++ huntingcoder/pages/api/getblog.js
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // http://localhost:3000/api/getblog?slug=how-to-learn-javascript
 import * as fs from "node:fs";
 
